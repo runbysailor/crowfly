776452e3fb072bea6f8efc09a3fc01898b0ccec5
runbysailor <admin@runbysailor.com>
2011-03-23 18:21:21 -0500

    Size and link image.

--- app/models/point.rb
+++ app/models/point.rb
@@ -3,5 +3,5 @@ class Point < ActiveRecord::Base
   validates_presence_of :name
   validates_uniqueness_of :name, :scope => [:viewer_id]
   validates_numericality_of :latitude, :longitude, :message => 'must be a number'
-  has_attached_file :image, :styles => { :original => ["300x300>"], :thumb => ["100x100>"] }
+  has_attached_file :image, :styles => { :cropped => ["300x300>"], :thumb => ["100x100>"] }
 end
