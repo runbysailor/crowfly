514da59d67089817e01d3af619dfd57292095243
runbysailor <admin@runbysailor.com>
2011-03-28 21:00:02 -0500

    Do not allow kmz.

--- app/models/geofile.rb
+++ app/models/geofile.rb
@@ -5,5 +5,5 @@ class Geofile < ActiveRecord::Base
   validates_presence_of :name
   validates_uniqueness_of :name, :scope => [:user_id]    
   has_attached_file :kml
-  validates_attachment_content_type :kml, :content_type => [ 'application/vnd.google-earth.kml+xml', 'application/vnd.google-earth.kmz' ], :message => 'is not appropriate. Please use a file ending in .kml or .kmz.'
+  validates_attachment_content_type :kml, :content_type => [ 'application/vnd.google-earth.kml+xml' ], :message => 'is not appropriate. Please use a file ending in .kml.'
 end
